db4e8252a6ddd1ae205233bb2222f8d3d99a431d
Sehee-Lee-01 <tpfktpgml24@kookmin.ac.kr>
2022-09-17 01:38:54 +0900

    environment: firebase init

---
+++ functions/.eslintrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+  root: true,
+  env: {
+    es8: true,
+    node: true,
+  },
+  extends: [
+    "eslint:recommended",
+    "google",
+  ],
+  rules: {
+    quotes: ["error", "double"],
+  },
+};
