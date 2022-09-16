74230385a2a248b1e10875e86a904ff9bbfe72a3
Junyong <dak_kiwon@DAk-kiwonui-MacBookPro.local>
2022-09-16 18:14:42 +0900

    add:firebase

--- carkeeper/lib/main.dart
+++ carkeeper/lib/main.dart
@@ -4,8 +4,14 @@ import 'package:carkeeper/pages/record_page.dart';
 import 'package:carkeeper/pages/stream_page.dart';
 import 'package:carkeeper/styles.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(MyApp());
 }
 
