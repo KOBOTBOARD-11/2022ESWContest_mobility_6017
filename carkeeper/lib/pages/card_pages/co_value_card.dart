cae861011aa63a5dfce4f27fe9e1bf71a3e70d9d
Yeonoc <chlqhtjr752@kookmin.ac.kr>
2022-09-19 19:17:11 +0900

    feature : gas sensor live stream

---
+++ carkeeper/lib/pages/card_pages/co_value_card.dart
@@ -0,0 +1,27 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+
+import '../commons/common_form_card.dart';
+
+class CoValueCard extends StatefulWidget {
+  @override
+  State<CoValueCard> createState() => _CoValueCardState();
+}
+
+class _CoValueCardState extends State<CoValueCard> {
+  var _snapshot = FirebaseFirestore.instance
+      .collection('gas_sensor')
+      .doc("detect_co")
+      .snapshots();
+  @override
+  Widget build(BuildContext context) {
+    return StreamBuilder(
+        stream: _snapshot,
+        builder: (BuildContext context, AsyncSnapshot<dynamic> snapshot) {
+          // print(snapshot.data?["ppm"]);
+          var amt = snapshot.data?["ppm"];
+          print(amt);
+          return CommonFormCard(gasName: "일산화탄소(CO)", amount: amt);
+        });
+  }
+}
---
+++ carkeeper/lib/pages/card_pages/lpg_value_card.dart
@@ -0,0 +1,27 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+
+import '../commons/common_form_card.dart';
+
+class LPGCard extends StatefulWidget {
+  @override
+  State<LPGCard> createState() => _LPGCardState();
+}
+
+class _LPGCardState extends State<LPGCard> {
+  var _snapshot = FirebaseFirestore.instance
+      .collection('gas_sensor')
+      .doc("detect_lpg")
+      .snapshots();
+  @override
+  Widget build(BuildContext context) {
+    return StreamBuilder(
+        stream: _snapshot,
+        builder: (BuildContext context, AsyncSnapshot<dynamic> snapshot) {
+          // print(snapshot.data?["ppm"]);
+          var amt = snapshot.data?["ppm"];
+          print(amt);
+          return CommonFormCard(gasName: "LPG", amount: amt);
+        });
+  }
+}
