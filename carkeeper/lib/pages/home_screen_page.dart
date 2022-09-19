3678004a07d56417321060ad31d01e06a09a3958
Sehee-Lee-01 <tpfktpgml24@kookmin.ac.kr>
2022-09-19 19:18:46 +0900

    feature:set firebase cloud messeging

--- carkeeper/lib/pages/home_screen_page.dart
+++ carkeeper/lib/pages/home_screen_page.dart
@@ -2,9 +2,12 @@ import 'package:carkeeper/components/home_page_components/record_components.dart
 import 'package:carkeeper/components/home_page_components/stream_components.dart';
 import 'package:carkeeper/styles.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 
 import '../components/home_page_components/co_make_components.dart';
+
 import '../components/home_page_components/face_components.dart';
+import '../main.dart';
 
 class HomeScreenPage extends StatefulWidget {
   @override
@@ -12,6 +15,25 @@ class HomeScreenPage extends StatefulWidget {
 }
 
 class _HomeScreenPageState extends State<HomeScreenPage> {
+  void initState() {
+    var initialzationSettingsAndroid =
+        AndroidInitializationSettings('@mipmap/ic_launcher');
+
+    var initialzationSettingsIOS = IOSInitializationSettings(
+      requestSoundPermission: true,
+      requestBadgePermission: true,
+      requestAlertPermission: true,
+    );
+    var initializationSettings = InitializationSettings(
+        android: initialzationSettingsAndroid, iOS: initialzationSettingsIOS);
+
+    flutterLocalNotificationsPlugin.initialize(
+      initializationSettings,
+      // onSelectNotification: selectionNotification,
+    );
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
