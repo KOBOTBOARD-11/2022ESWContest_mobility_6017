9f02574cc014e8e41f696e61d3b124159b6fb150
Sehee-Lee-01 <tpfktpgml24@kookmin.ac.kr>
2022-09-29 23:48:17 +0900

    feat : 화면 정렬 및 버튼 수정

--- carkeeper/lib/pages/stream_page.dart
+++ carkeeper/lib/pages/stream_page.dart
@@ -1,4 +1,5 @@
 import 'package:carkeeper/pages/record_page.dart';
+import 'package:date_format/date_format.dart';
 import 'package:flutter/material.dart';
 import 'package:carkeeper/components/stream_page_components/stream_video.dart';
 import '../styles.dart';
@@ -7,43 +8,34 @@ class StreamPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(
-          "Car Keeper",
-          style: h5(mColor: Color(0xFF06A66C)),
-        ),
-      ),
-      body: ListView(
-        children: [
-          VideoStream(),
-          Container(
-            padding: EdgeInsets.symmetric(vertical: 10),
-            alignment: Alignment.center,
-            constraints: BoxConstraints(maxWidth: 300),
-            child: Text(
-              "※ 객체가 탐지되면 객체의 움직임을\n\t\t\t 감지합니다.",
-              style: subtitle3(),
-            ),
+        appBar: AppBar(
+          title: Text(
+            "Car Keeper",
+            style: h5(mColor: Color(0xFF06A66C)),
           ),
-          Spacer(),
-          Container(
-            width: double.infinity,
-            height: 85,
-            color: Color(0xFF06A66C),
-            child: TextButton(
-                onPressed: () {
-                  Navigator.of(context).push(MaterialPageRoute(
-                      builder: (context) => const RecordPage()));
-                },
-                child: Text(
-                  "접근 기록 확인하기",
-                  style: h5(
-                    mColor: Colors.white,
-                  ),
-                )),
-          )
-        ],
-      ),
-    );
+        ),
+        body: Center(
+            child: ListView(
+          children: [
+            Container(
+              padding: EdgeInsets.only(top: 10),
+              alignment: Alignment.center,
+              constraints: BoxConstraints(maxWidth: 300),
+              child: Text(
+                "※ 객체가 탐지되면 객체의 움직임을 감지합니다.",
+                style: subtitle1(),
+              ),
+            ),
+            VideoStream(),
+          ],
+        )),
+        floatingActionButton: FloatingActionButton.extended(
+            onPressed: () {
+              Navigator.of(context).push(
+                  MaterialPageRoute(builder: (context) => const RecordPage()));
+            },
+            label: Text(
+              "접근 기록 확인하기",
+            )));
   }
 }
