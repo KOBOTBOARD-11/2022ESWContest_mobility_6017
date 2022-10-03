211ddc26accc06a4dc091b843d16b651445d8b27
Sehee-Lee-01 <tpfktpgml24@kookmin.ac.kr>
2022-10-03 18:14:39 +0900

    feat: 좌표 메세지 받는 방법 json

--- raspberry_servo_track/servo.py
+++ raspberry_servo_track/servo.py
@@ -1,6 +1,13 @@
+from email import message
+from operator import truediv
 import paho.mqtt.client as mqtt
+import json 
 import RPi.GPIO as GPIO
 import time
+
+global obj_flag
+global x_flag
+global y_flag
 
 def on_connect(client, userdata, flags, rc):
     if rc == 0:
@@ -14,9 +21,19 @@ def on_disconnect(client, userdata, flags, rc=0):
 def on_subscribe(client, userdata, mid, granted_qos):
     print("subscribed: " + str(mid) + " " + str(granted_qos))
 
-# 서버가 
 def on_message(client, userdata, msg):
     print(str(msg.payload.decode("utf-8")))
+    message = str(msg.payload.decode("utf-8"))
+    # 객체 탐지 유무:obj_flag, x방향, y방향
+    #obj_flag = message.
+    json_data = json.loads(message)
+    obj_flag = json_data["obj_flag"]
+    if obj_flag: # 객체가 탐지 되었을 때,
+        x_flag= json_data["x_flag"]; # 센터 - 객체 센터(양: 왼쪽, 음: 오른쪽)
+        y_flag= json_data["y_flag"]; # 센터 - 객체센터(양: 위쪽, 음: 아래쪽)
+    else:
+        x_flag=0; # 센터 - 객체 센터(양: 왼쪽, 음: 오른쪽)
+        y_flag=0;
  
 client = mqtt.Client()
 
@@ -27,10 +44,15 @@ client.on_message = on_message
 
 client.connect('localhost', 1883)
 client.subscribe('test/hello', 1)
+client.loop_start()
 
-# direction: 1 or -1
-x_flag = 1
-# y_flag = 1
+obj_flag=False;
+x_flag=1;
+y_flag=1;
+
+# direction
+x_direc = [-1,1] # -1: , 1:
+y_direc = [-1,1] # -1: , 1:
 
 servo_x_pin = 18
 servo_y_pin = 23
@@ -44,16 +66,20 @@ pwm_y = GPIO.PWM(servo_y_pin, 50)
 pwm_x.start(3.0)
 pwm_y.start(3.0)
 
-for i in range (0,3) :    
-    for high_time in range (30, 125):
-        pwm_x.ChangeDutyCycle(high_time/10.0) # for 반복문에 실수가 올 수 없으므로 /10.0 로 처리함. 
-        pwm_y.ChangeDutyCycle(high_time/10.0) # for 반복문에 실수가 올 수 없으므로 /10.0 로 처리함. 
-        time.sleep(0.02)
- 
-    for high_time in range (124, 30,-1):
-        pwm_x.ChangeDutyCycle(high_time/10.0) # for 반복문에 실수가 올 수 없으므로 /10.0 로 처리함. 
-        pwm_y.ChangeDutyCycle(high_time/10.0) # for 반복문에 실수가 올 수 없으므로 /10.0 로 처리함. 
-        time.sleep(0.02)
+high_x_time = 30
+high_y_time = 30
+
+while True:
+    time.sleep(0.02)
+    pos_x = x_direc[x_flag]*high_x_time
+    pos_y = y_direc[y_flag]*high_y_time
+    if (obj_flag != ""):
+        pwm_x.ChangeDutyCycle(pos_x/10.0) # for 반복문에 실수가 올 수 없으므로 /10.0 로 처리함. 
+        pwm_y.ChangeDutyCycle(pos_y/10.0) # for 반복문에 실수가 올 수 없으므로 /10.0 로 처리함. 
+
+    else:
+        pwm_x.ChangeDutyCycle(pos_x/10.0) # for 반복문에 실수가 올 수 없으므로 /10.0 로 처리함. 
+        pwm_y.ChangeDutyCycle(pos_y/10.0) # for 반복문에 실수가 올 수 없으므로 /10.0 로 처리함. 
 
 pwm_x.ChangeDutyCycle(0.0)
 pwm_y.ChangeDutyCycle(0.0)
@@ -62,5 +88,4 @@ pwm_x.stop()
 pwm_y.stop()
 
 GPIO.cleanup()
-
-client.loop_forever()
+client.loop_stop()
