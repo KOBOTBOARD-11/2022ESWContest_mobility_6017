5430acfde9fb87339bd966d0065a7f286eb548b0
Sehee-Lee-01 <tpfktpgml24@kookmin.ac.kr>
2022-10-08 19:58:53 +0900

    refactor: 문서 정리

---
+++ rasp_web_stream/server.py
@@ -0,0 +1,31 @@
+from flask import Flask, render_template, Response
+import cv2
+from time import sleep
+
+app = Flask(__name__)
+capture = cv2.VideoCapture(0)
+capture.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
+capture.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
+
+def gen_frames():  
+    while True:
+        sleep(0.1)
+        ref, frame = capture.read()
+        if not ref:
+            break
+        else:
+            ref, buffer = cv2.imencode('.jpg', frame)
+            frame = buffer.tobytes()
+            yield (b'--frame\r\n'
+                   b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/video_feed')
+def video_feed():
+    return Response(gen_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
+
+if __name__ == "__main__":
+   app.run(host="10.3.60.134", port = "8080")
