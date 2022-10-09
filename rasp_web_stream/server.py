9da54428abd3bbb457bf6ea9489ca494b8363cdb
Hailey Lee <85275893+Sehee-Lee-01@users.noreply.github.com>
2022-10-10 02:36:38 +0900

    Update server.py

--- rasp_web_stream/server.py
+++ rasp_web_stream/server.py
@@ -7,7 +7,7 @@ capture = cv2.VideoCapture(0)
 capture.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
 capture.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
 
-def gen_frames():  
+def generate_frames():  
     while True:
         sleep(0.1)
         ref, frame = capture.read()
@@ -25,7 +25,7 @@ def index():
 
 @app.route('/video_feed')
 def video_feed():
-    return Response(gen_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
+    return Response(generate_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
 
 if __name__ == "__main__":
    app.run(host="10.3.60.134", port = "8080")
